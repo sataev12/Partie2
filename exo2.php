b1d8bec10b7bff5687d7d76f14e889e93e956ab5
John Doe11 <sataevmagomed.eu@gmail.com>
2024-01-30 02:26:36 +0100

    Partie2 PHP Exo2 terminée

--- exo2.php
+++ exo2.php
@@ -2,9 +2,54 @@
 
 <p>Soit le tableau suivant : <code>$capitales = ["France"=>"Paris", "Allemagne"=>"Berlin",
     "USA"=>"Washington", "Italie"=>"Rome"];</code><br>
-Réaliser un algorithme permettant d'afficher le tableau HTML suivant(notez que le nom du pays s'affichera en majuscule et que le tableau est trié par ordre alphabétique du nom du pays) grâce à une fonction personalisée.</p>
+Réaliser un algorithme permettant d'afficher le tableau HTML suivant(notez que le nom du pays
+s'affichera en majuscule et que le tableau est trié par ordre alphabétique du nom du pays) grâce
+à une fonction personalisée.</p>
 
 <h2>Résultat</h2>
 
+<!-- <table> 
+    <thead>
+        <tr> 
+            <th>Pays</th> 
+            <th>Capitale</th> 
+        </tr>
+    </thead>
+
+    <tr> 
+        <td>France</td> 
+        <td>Paris</td> 
+    </tr>
+</table> -->
+
 <?php
+
+$capitales = array(
+    "France"=>"Paris",
+    "Allemagne"=>"Berlin",
+    "USA"=>"Washington",
+    "Italie"=>"Rome"
+);
+
+function afficherTableHTML($capitales){
+    ksort($capitales);
+    $result = "<table border = 1> 
+                    <thead>
+                        <tr> 
+                            <th>Pays</th> 
+                            <th>Capitale</th> 
+                        </tr>
+                    </thead>";
+    foreach ($capitales as $pays => $capitale) {
+        $result .= "<tr> 
+                        <td>".mb_strtoupper($pays)."</td> 
+                        <td>".ucfirst($capitale)."</td> 
+                    </tr>";
+    }
+    $result .= "</table>";
+    
+    return $result;
+}
+
+echo afficherTableHTML($capitales);
 
