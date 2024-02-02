d742d0305af52f0d53fbcf4f3981798b27b2079c
John Doe11 <sataevmagomed.eu@gmail.com>
2024-02-02 11:10:20 +0100

    Exo13 terminé partie 2 PHP

--- exo4.php
+++ exo4.php
@@ -1,48 +1,29 @@
 <h1>Exercice 4</h1>
 
-<p>A partir de l'exercice 2, ajouter une colonne supplémentaire dans le tableau HTML qui contiendra le lien 
-    hypertexte de la page Wikipédia de la capitale(le lien hypertexte devra s'ouvrir dans un nouvel
-    onglet et le tableau sera trié par ordre alphabétique de la capitale)<br>.
-
-    On admet que l'URL de la page Wikipédia de la capitale adopte la forme: hhtps://fr.wikipedia.org/wiki/
-</p>
-
+<p>Ecrire un algorithme permettant de savoir si une phrase est palindrome.
 
 <h2>Résultat</h2>
 
+<p>Affichage :
+</p> 
+
 <?php
 
-$capitales = array(
-    "France"=>"Paris",
-    "Allemagne"=>"Berlin",
-    "USA"=>"Washington",
-    "Italie"=>"Rome",
-    "Espagne"=>"Madrid"
-);
+$phraseOri = "Engage le jeu que je le gagne";
 
-$urlLien = "https://fr.wikipedia.org/wiki/";
+$phrase1 = str_replace(" ","",strtolower($phraseOri));
 
-function afficherTableHTML($capitales, $urlLien){
-    
-    asort($capitales);
-    $result = "<table border = 1> 
-                    <thead>
-                        <tr> 
-                            <th>Pays</th> 
-                            <th>Capitale</th>
-                            <th>Lien wiki</th> 
-                        </tr>
-                    </thead>";
-    foreach ($capitales as $pays => $capitale) {
-        $result .= "<tr> 
-                        <td>".mb_strtoupper($pays)."</td> 
-                        <td>".ucfirst($capitale). "</td> 
-                        <td><a href=\"$urlLien/$capitale\" target=\"_blank\">Lien</a></td>
-                    </tr>";
-    }
-    $result .= "</table>";
-    
-    return $result;
+$phrase2 = strrev($phrase1);
+
+if ($phrase1 == $phrase2) {
+
+    echo "La phrase \"$phraseOri\" est palindrome ";
+
+}
+else {
+
+    echo "La phrase \"$phraseOri\" n'est pas palindrome ";
+
 }
 
-echo afficherTableHTML($capitales, $urlLien);
+?>
--- exo5.php
+++ exo5.php
@@ -1,46 +1,16 @@
 <h1>Exercice 5</h1>
 
-<p>Crée une fonction personalisée permettant de créer un formulaire de champs de texte en précisant le nom 
-    des champs associés.
-</p>
+<p>Ecrire un algorithme qui déclare une valeur en francs et qui la convertit en euros. 
+Attention, la valeur générée devra être arrondie à 2 décimale
+</p> 
 
 <h2>Résultat</h2>
 
-<!-- <form action="" method="get">
-    <label for="nom">Nom</label>
-    <input type="text" name="nom" id="nom" size="20" maxlength="20">
-
-    <label for="prenom">Prenom</label>
-    <input type="text" name="prenom" id="prenom" size="20" maxlength="20">
-
-    <label for="ville">Ville</label>
-    <input type="text" name="ville" id="ville" size="20" maxlength="20">
-</form> -->
-
 <?php
+$montantFrancs = 75;
+$conversionEuro = 0.1524;
+$resultat = round($montantFrancs * $conversionEuro,2);
 
-$nomsInput = [
-    "Nom", 
-    "Prénom", 
-    "Ville"
-];
-    
-
-
-function afficherInput($nomsInput){
-    $result = '<form action="" method="get">';
-
-    foreach ($nomsInput as $key) {
-        $result.='
-                    <label for="'.$key.'">' .$key .'</label>
-                    <input type="text" name="' .$key .'" size="20" maxlength="20">';
-    }
-
-    $result.="</form>";
-
-
-    return $result;
-}
-
-echo afficherInput($nomsInput);
-
+echo "Affichage :<br><br> Montant en francs : $montantFrancs<br>
+$montantFrancs francs = $resultat €";
+?>
